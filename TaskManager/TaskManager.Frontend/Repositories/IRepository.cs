0f1659e69715f69d01f9f691003f1cf63436eb71
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-06-02 11:52:53 -0500

    Begin create a task

--- TaskManager/TaskManager.Frontend/Repositories/IRepository.cs
+++ TaskManager/TaskManager.Frontend/Repositories/IRepository.cs
@@ -5,5 +5,11 @@ namespace TaskManager.Frontend.Repositories
     public interface IRepository
     {
         Task<Response<T>> GetAsync<T>(string url);
+
+        Task<Response<T>> PostAsync<T>(string url, T model);
+
+        Task<Response<T>> PutAsync<T>(string url, T model);
+
+        Task<Response<T>> DeleteAsync<T>(string url);
     }
 }
--- TaskManager/TaskManager.Frontend/Repositories/Repository.cs
+++ TaskManager/TaskManager.Frontend/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using TaskManager.Shared.Models;
 
 namespace TaskManager.Frontend.Repositories
@@ -35,6 +36,68 @@ namespace TaskManager.Frontend.Repositories
             {
                 IsSuccess = true,
                 Result = responseJson
+            };
+        }
+
+        public async Task<Response<T>> PostAsync<T>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model, _jsonDefaultOptions);
+            var messageContet = new StringContent(messageJson, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PostAsync(url, messageContet);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to post the object"
+                };
+            }
+            var responseString = await responseHttp.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<T>(responseString, _jsonDefaultOptions);
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Result = responseJson
+            };
+        }
+
+        public async Task<Response<T>> PutAsync<T>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model, _jsonDefaultOptions);
+            var messageContet = new StringContent(messageJson, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContet);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to put the object"
+                };
+            }
+            var responseString = await responseHttp.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<T>(responseString, _jsonDefaultOptions);
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Result = responseJson
+            };
+        }
+
+        public async Task<Response<T>> DeleteAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.DeleteAsync(url);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to delete the object"
+                };
+            }
+
+            return new Response<T>
+            {
+                IsSuccess = true,
             };
         }
     }
